09f63832b79eee424d41ec8734b10ea5d23cbc30
Riley Moore <rilemoore@gmail.com>
2019-11-28 21:31:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
   
   configure do
@@ -9,6 +9,11 @@ class App < Sinatra::Base
   
   get '/' do
     erb :index
+  end
+  
+  post '/checkout' do
+    @sessions = session
+    
   end
   
 end
